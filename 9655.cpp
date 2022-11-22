7f4d13714522d811dc2b283b4e17efb91ac40321
ddd2341 <rlawnsrud0509@gmail.com>
2022-11-22 14:35:20 +0900

    wefwefwe

---
+++ 9655.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+  cout << fixed;
+
+  int a;
+  cin >> a;
+  a%2==0?cout<<"CY":cout<<"SK";
+
+  return 0;
+}
