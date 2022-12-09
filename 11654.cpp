559cb2c28bee6cbb590d923fff31742ec1432c6f
ddd2341 <rlawnsrud0509@gmail.com>
2022-12-09 14:11:07 +0900

    씨쁠쁠다시 시작했어용 ㅎㅎ

---
+++ 11654.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  char a;
+  cin >> a;
+
+  printf("%d", a);
+
+  return 0;
+}
---
+++ 2675.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+  ios_base :: sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int a, rep;
+  char arr[21];
+
+  cin >> a;
+
+  for(int i=0; i<a; i++) {
+    cin >> rep;
+    cin >> arr;
+
+    for(int j=0; j<strlen(arr); j++) {
+      for(int k=0; k<rep; k++) {
+        cout << arr[j];
+      }
+    }
+    cout << "\n";
+  }
+
+  return 0;
+}
